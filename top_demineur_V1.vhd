09c2f09ae4da7b63cc287169ffa62f550dcf34f0
Florian Guyot <28florianguyot@gmail.com>
2016-04-27 23:22:58 +0200

    Add all file in top level, decode env and generation are not available

--- top_demineur_V1.vhd
+++ top_demineur_V1.vhd
@@ -39,8 +39,10 @@ entity top_demineur_V1 is
 				down 				: in  STD_LOGIC;
 				discover_bp 	: in  STD_LOGIC;
 				en1				: in STD_LOGIC;
+				CE_env			: in STD_LOGIC;
 				value				: in STD_LOGIC_VECTOR (3 downto 0);
 				LEDF 				: out STD_LOGIC_VECTOR (5 downto 0);
+				led_env 				: out STD_LOGIC;
 				vga_hs 			: out  STD_LOGIC;
 				vga_vs 			: out  STD_LOGIC;
 				vga_red 			: out  STD_LOGIC_VECTOR (3 downto 0);
@@ -83,7 +85,6 @@ component top_grille_test is
 end component top_grille_test;
 
 
-
 component cadenceur is
     Port ( clk : in  STD_LOGIC;
            rst : in  STD_LOGIC;
@@ -97,11 +98,60 @@ component anti_bounding is
            output : out  STD_LOGIC);
 end component anti_bounding;
 
+component top_create_grid is
+    Port ( 			--input : in  STD_LOGIC_VECTOR (9 downto 0);          
+				clock : in  STD_LOGIC;
+				--load : in STD_LOGIC;
+				--ce : in  STD_LOGIC;
+				reset : in  STD_LOGIC;
+				output_l0 : out  STD_LOGIC_VECTOR (9 downto 0);
+				output_l1 : out  STD_LOGIC_VECTOR (9 downto 0);
+				output_l2 : out  STD_LOGIC_VECTOR (9 downto 0);
+				output_l3 : out  STD_LOGIC_VECTOR (9 downto 0);
+				output_l4 : out  STD_LOGIC_VECTOR (9 downto 0);
+				output_l5 : out  STD_LOGIC_VECTOR (9 downto 0);
+				output_l6 : out  STD_LOGIC_VECTOR (9 downto 0);
+				output_l7 : out  STD_LOGIC_VECTOR (9 downto 0);
+				output_l8 : out  STD_LOGIC_VECTOR (9 downto 0);
+				output_l9 : out  STD_LOGIC_VECTOR (9 downto 0));
+end component top_create_grid;
+
+
+component top_decode_env is
+    Port ( 	clk : in  STD_LOGIC;
+          	Ce : in  STD_LOGIC;
+           	rst : in  STD_LOGIC;
+           	line0 : in  STD_LOGIC_VECTOR (9 downto 0);
+				line1 : in  STD_LOGIC_VECTOR (9 downto 0);
+				line2 : in  STD_LOGIC_VECTOR (9 downto 0);
+				line3 : in  STD_LOGIC_VECTOR (9 downto 0);
+				line4 : in  STD_LOGIC_VECTOR (9 downto 0);
+				line5 : in  STD_LOGIC_VECTOR (9 downto 0);
+				line6 : in  STD_LOGIC_VECTOR (9 downto 0);
+				line7 : in  STD_LOGIC_VECTOR (9 downto 0);
+				line8 : in  STD_LOGIC_VECTOR (9 downto 0);
+				line9 : in  STD_LOGIC_VECTOR (9 downto 0);
+
+				add_memo: out  STD_LOGIC_VECTOR(6 downto 0);
+				data_memo : out  STD_LOGIC_VECTOR(5 downto 0);
+				en_memo : out  STD_LOGIC;
+				read_write_memo : out  STD_LOGIC;
+				end_process_decod: out  STD_LOGIC);
+  
+end component top_decode_env;
+
 
 
 
 signal sig_pos_lr, sig_pos_ud : STD_LOGIC_VECTOR (3 downto 0);
 signal sig_ce_bp,sig_dis_bp : STD_LOGIC;
+
+signal sig_add_mem_matrice_in : STD_LOGIC_VECTOR (6 downto 0);
+signal sig_data_mem_matrice_in : STD_LOGIC_VECTOR (5 downto 0);
+signal sig_en_mem_matrice_in : STD_LOGIC;
+signal sig_rw_mem_matrice_in : STD_LOGIC;
+
+signal sig_mem_l0,sig_mem_l1,sig_mem_l2,sig_mem_l3,sig_mem_l4,sig_mem_l5,sig_mem_l6,sig_mem_l7,sig_mem_l8,sig_mem_l9 : STD_LOGIC_VECTOR (9 downto 0);
 
 begin
 
@@ -143,7 +193,43 @@ ab_disc : anti_bounding port map (  	clock => clk ,
 													reset =>  rst,
 													enable =>  discover_bp,
 													output =>  sig_dis_bp);	
-									
+													
+Create_grid : top_create_grid port map (  	--input : in  STD_LOGIC_VECTOR (9 downto 0);          
+															clock => clk ,
+															--load : in STD_LOGIC;
+															--ce : in  STD_LOGIC;
+															reset =>  rst,
+															output_l0 =>  sig_mem_l0,
+															output_l1 =>  sig_mem_l1,
+															output_l2 =>  sig_mem_l2,
+															output_l3 =>  sig_mem_l3,
+															output_l4 =>  sig_mem_l4,
+															output_l5 =>  sig_mem_l5,
+															output_l6 =>  sig_mem_l6,
+															output_l7 =>  sig_mem_l7,
+															output_l8 =>  sig_mem_l8,
+															output_l9 =>  sig_mem_l9);
+															
+
+Decode_grid : top_decode_env port map (  	clk => clk ,
+														Ce => CE_env , -- a changer
+														rst =>  rst,
+														line0 =>  sig_mem_l0,
+														line1 =>  sig_mem_l1,
+														line2 =>  sig_mem_l2,
+														line3 =>  sig_mem_l3,
+														line4 =>  sig_mem_l4,
+														line5 =>  sig_mem_l5,
+														line6 =>  sig_mem_l6,
+														line7 =>  sig_mem_l7,
+														line8 =>  sig_mem_l8,
+														line9 =>  sig_mem_l9,
+
+														add_memo => sig_add_mem_matrice_in,
+														data_memo => sig_data_mem_matrice_in,
+														en_memo => sig_en_mem_matrice_in,
+														read_write_memo => sig_rw_mem_matrice_in,
+														end_process_decod => led_env); -- a changer
 
 
 end Behavioral;
